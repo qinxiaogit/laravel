d106e6c9f7e85881b387403e5811bffc17bb87ac
qinxiaogit <qinxiaolove@gmail.com>
2016-10-23 23:02:40 +0800

    学习view

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,7 +14,7 @@
 //Route::get('/', function () {
 //    return view('welcome');
 //});
-Route::get('/', 'HomeController@index');
+//Route::get('/', 'HomeController@index');
 /*
 |--------------------------------------------------------------------------
 | Application Routes
@@ -26,81 +26,85 @@ Route::get('/', 'HomeController@index');
 |
 */
 
-Route::group(['middleware' => ['web']], function () {
-    //
-});
-
-//get
-Route::get('base',function(){
-	return 'Hello World';
-});
-//post
-Route::post('base2',function(){
-	return 'Hello World';
-});
-
-//多请求路由
-Route::match(['get','post'],'multy1',function(){
-	return 'multy1';
-});
-
-Route::any('multy2',function(){
-	return 'multy2';
-});
-//路由参数
-// Route::get('user/{id}',function($id){
-
-// return 'user->id'.$id;
+// Route::group(['middleware' => ['web']], function () {
+//     //
 // });
 
-// Route::get('user/{name?}',function($name){
+// //get
+// Route::get('base',function(){
+// 	return 'Hello World';
+// });
+// //post
+// Route::post('base2',function(){
+// 	return 'Hello World';
+// });
 
-// return 'user->name:'.$name;
-// })->where('name','[A-Za-z]+');
-// Route::get('user/{id}/{name?}',function($id,$name){
+// //多请求路由
+// Route::match(['get','post'],'multy1',function(){
+// 	return 'multy1';
+// });
 
-// 	return 'user->id'.$id.'->name:'.$name;
-// })->where(['id'=>'[0-9]+','name'=>'[A-Za-z]+']);
+// Route::any('multy2',function(){
+// 	return 'multy2';
+// });
+// //路由参数
+// // Route::get('user/{id}',function($id){
 
-//路由别名
-Route::get('user/center',['as'=>'center',function(){
-	return route('center');
-}]);
-//路由群组
-Route::group(['prefix'=>'member'],function(){
-	Route::get('user/center',['as'=>'center',function(){
-		return route('center');
-	}]);
-	Route::any('multy2',function(){
-		return 'multy3';
-	});
+// // return 'user->id'.$id;
+// // });
+
+// // Route::get('user/{name?}',function($name){
+
+// // return 'user->name:'.$name;
+// // })->where('name','[A-Za-z]+');
+// // Route::get('user/{id}/{name?}',function($id,$name){
+
+// // 	return 'user->id'.$id.'->name:'.$name;
+// // })->where(['id'=>'[0-9]+','name'=>'[A-Za-z]+']);
+
+// //路由别名
+// Route::get('user/center',['as'=>'center',function(){
+// 	return route('center');
+// }]);
+// //路由群组
+// Route::group(['prefix'=>'member'],function(){
+// 	Route::get('user/center',['as'=>'center',function(){
+// 		return route('center');
+// 	}]);
+// 	Route::any('multy2',function(){
+// 		return 'multy3';
+// 	});
+// });
+
+// //路由输出视图
+// Route::get('view',function(){
+// 	return view('welcome');
+// });
+
+// Route::get('/testUrl','HomeController@testUrl');
+// Route::group(['middleware'=>['web']],function (){
+//     Route::any('sesion',['uses'=>'HomeController@sesion']);
+//     Route::any('sesiona',[
+//         'as'=>'xm',
+//         'uses'=>'HomeController@sesiona']);
+//     Route::any('response',['uses'=>'HomeController@response']);
+// });
+// Route::get('/active0','HomeController@active0');
+// Route::group(['middleware'=>['active']],function(){
+//     Route::get('/active1','HomeController@active1');
+//     Route::get('/active2','HomeController@active2');
+// });
+
+
+// //Route::group(['middleware' => 'web'], function () {
+// //    Route::auth();
+// //
+// //    Route::get('/home', 'HomeController@index');
+// //});
+
+// //Route::get('/student/','StudentController@index');
+// Route::get('/student/',['uses'=>'StudentController@index']);
+
+Route::get('/',function(){
+	return view('home');
 });
-
-//路由输出视图
-Route::get('view',function(){
-	return view('welcome');
-});
-
-Route::get('/testUrl','HomeController@testUrl');
-Route::group(['middleware'=>['web']],function (){
-    Route::any('sesion',['uses'=>'HomeController@sesion']);
-    Route::any('sesiona',[
-        'as'=>'xm',
-        'uses'=>'HomeController@sesiona']);
-    Route::any('response',['uses'=>'HomeController@response']);
-});
-Route::get('/active0','HomeController@active0');
-Route::group(['middleware'=>['active']],function(){
-    Route::get('/active1','HomeController@active1');
-    Route::get('/active2','HomeController@active2');
-});
-
-
-//Route::group(['middleware' => 'web'], function () {
-//    Route::auth();
-//
-//    Route::get('/home', 'HomeController@index');
-//});
-
-//Route::get('/student/','StudentController@index');
-Route::get('/student/',['uses'=>'StudentController@index']);
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,12 @@
-@extends('layouts.app')
+@extends('master')
 
+@section('title','登录')
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
-
-                <div class="panel-body">
-                    You are logged in!
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+    <p>登录</p>
+@endsection
+@section('my-js')
+<script type="text/javascript">
+    
+    alert('AAA');
+</script>
 @endsection
